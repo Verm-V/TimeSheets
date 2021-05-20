215962a4d3505e9990c2245d02c92ca15972707c
Verm-V <Verm-V@yandex.ru>
2021-05-21 01:46:39 +0600

    Добавлено логгирование через NLog

--- TimeSheets/Program.cs
+++ TimeSheets/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,42 @@ namespace TimeSheets
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+			try
+			{
+				logger.Trace("---- [BEGIN]----");
+				logger.Debug("---- [BEGIN]----");
+				logger.Info("---- [BEGIN]----");
+				CreateHostBuilder(args).Build().Run();
+			}
+			// отлов всех исключений в рамках работы приложения
+			catch (Exception exception)
+			{
+				// NLog: устанавливаем отлов исключений
+				logger.Error(exception, "Stopped program because of exception");
+			}
+			finally
+			{
+				// Остановка логера 
+				logger.Info("---- [ END ]----");
+				logger.Debug("---- [ END ]----");
+				logger.Trace("---- [ END ]----");
+				NLog.LogManager.Shutdown();
+			}
 		}
-
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
-				});
+				})
+				.ConfigureLogging(logging =>
+				{
+					logging.AddDebug();
+					logging.ClearProviders(); // Создание провайдеров логирования
+					logging.SetMinimumLevel(LogLevel.Trace); // Устанавливаем минимальный уровень логирования
+				})
+				.UseNLog(); // Добавляем библиотеку nlog
+
 	}
 }
